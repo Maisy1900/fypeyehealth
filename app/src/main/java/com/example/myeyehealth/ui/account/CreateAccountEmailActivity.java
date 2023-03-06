58b2a709ae2840434454771888e869ef99339cc8
Maisy <190041197@aston.ac.uk>
2023-03-06 00:41:12 +0000

    adding functionality

--- app/src/main/java/com/example/myeyehealth/ui/account/CreateAccountEmailActivity.java
+++ app/src/main/java/com/example/myeyehealth/ui/account/CreateAccountEmailActivity.java
@@ -2,15 +2,42 @@ package com.example.myeyehealth.ui.account;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.myeyehealth.R;
 
 public class CreateAccountEmailActivity extends AppCompatActivity {
+    private EditText emailInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_email);
+
+        emailInput = findViewById(R.id.email_input);
+        Button confirmButton = findViewById(R.id.confirm_button);
+        ImageButton backButton = findViewById(R.id.back_button);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailInput.getText().toString();
+                // TODO: Validate the email input and proceed to the next activity
+                Intent intent = new Intent(CreateAccountEmailActivity.this, CreateAccountPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myeyehealth/ui/account/CreateAccountPasswordActivity.java
+++ app/src/main/java/com/example/myeyehealth/ui/account/CreateAccountPasswordActivity.java
@@ -2,15 +2,43 @@ package com.example.myeyehealth.ui.account;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.myeyehealth.R;
 
 public class CreateAccountPasswordActivity extends AppCompatActivity {
 
+    private EditText passwordInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_password);
+
+        passwordInput = findViewById(R.id.password_input);
+        Button confirmButton = findViewById(R.id.confirm_button);
+        ImageButton backButton = findViewById(R.id.back_button);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = passwordInput.getText().toString();
+                // TODO: Validate the password input and proceed to the next activity
+                Intent intent = new Intent(CreateAccountPasswordActivity.this, CreateAccountConfirmPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
