413dd02a1d4642c8b0cf4980db93249475c83949
Maisy <190041197@aston.ac.uk>
2023-03-06 23:26:38 +0000

    minght hasve to change it alol

--- app/src/main/java/com/example/myeyehealth/MainActivity.java
+++ app/src/main/java/com/example/myeyehealth/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.myeyehealth;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,69 +8,35 @@ import android.widget.TextView;
 
 import com.example.myeyehealth.model.DatabaseConnection;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button testConnectionButton;
     private TextView connectionResultTextView;
-    private DatabaseConnection dbConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get references to UI elements
         testConnectionButton = findViewById(R.id.test_connection_button);
         connectionResultTextView = findViewById(R.id.connection_result_text_view);
-        dbConnection = new DatabaseConnection();
 
+        // Set click listener for testConnectionButton
         testConnectionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String result = testConnection();
-                if (result != null) {
-                    connectionResultTextView.setText(result);
+            public void onClick(View v) {
+                // Test database connection
+                DatabaseConnection databaseConnection = new DatabaseConnection();
+                Connection connection = databaseConnection.connect();
+                if (connection != null) {
+                    connectionResultTextView.setText("Connection Successful");
+                    databaseConnection.disconnect(connection);
                 } else {
-                    connectionResultTextView.setText("Connection failed");
+                    connectionResultTextView.setText("Connection Failed");
                 }
             }
         });
-    }
-
-    private String testConnection() {
-        String url = "http://localhost/fypdb/dbconnect.php";
-        HttpURLConnection connection = null;
-        try {
-            // Open connection
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-
-            // Read response
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = in.readLine()) != null) {
-                response.append(line);
-            }
-            in.close();
-
-            // Return response
-            return response.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
     }
 }
--- app/src/main/java/com/example/myeyehealth/model/DatabaseConnection.java
+++ app/src/main/java/com/example/myeyehealth/model/DatabaseConnection.java
@@ -1,35 +1,20 @@
 package com.example.myeyehealth.model;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String DB_URL = "http://localhost/fypdb/dbconnect.php";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
 
     public Connection connect() {
         Connection connection = null;
         try {
-            // Create a request to the PHP file
-            URL url = new URL(DB_URL);
-            HttpURLConnection connectionRequest = (HttpURLConnection) url.openConnection();
-            connectionRequest.setRequestMethod("GET");
-            connectionRequest.connect();
-
-            // Check if the connection was successful
-            if (connectionRequest.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                // Get the connection string from the PHP file's response
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connectionRequest.getInputStream()));
-                String connectionString = reader.readLine();
-
-                // Connect to the database
-                connection = DriverManager.getConnection(connectionString);
-            }
-        } catch (Exception e) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
         return connection;
@@ -45,3 +30,4 @@ public class DatabaseConnection {
         }
     }
 }
+
